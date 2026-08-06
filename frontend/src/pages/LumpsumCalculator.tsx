a0f22f0b20e2b51a5dda2f3fe94c119cde829926
InderjotSingh5239 <inderjotsingharora31@gmail.com>
2026-08-06 18:58:51 +0530

    Update LumpsumCalculator.tsx

--- frontend/src/pages/LumpsumCalculator.tsx
+++ frontend/src/pages/LumpsumCalculator.tsx
@@ -88,11 +88,12 @@ useEffect(() => {
             </CardHeader>
             <CardContent>
               <ResponsiveContainer width="100%" height={280}>
-               <AreaChart data={(result.yearly_breakdown as any[]).map((item) => ({ 
-                            year: item.year, 
-                            value: item.value,
-                          }))}
-                          margin={{ top: 8, right: 8, left: 0, bottom: 0 }}
+               <AreaChart 
+                 data={(result.yearly_breakdown as any[]).map((item) => ({ 
+                    year: item.year, 
+                    value: item.value,
+                }))}
+                margin={{ top: 8, right: 8, left: 0, bottom: 0 }}
               >
                   <defs>
                     <linearGradient id="lumpValue" x1="0" y1="0" x2="0" y2="1">
